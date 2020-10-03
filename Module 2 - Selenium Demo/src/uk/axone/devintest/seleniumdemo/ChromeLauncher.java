e557e60e763bd54061c37071ef5d94aea2459c27
Priya <priya3kr@gmail.com>
2020-10-03 10:14:43 +0100

    TestAutomation Project - Commiting Module 2 - Selenium ChromeLauncher Class 2

---
+++ Module 2 - Selenium Demo/src/uk/axone/devintest/seleniumdemo/ChromeLauncher.java
@@ -0,0 +1,17 @@
+package uk.axone.devintest.seleniumdemo;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeLauncher {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+
+        ChromeDriver cd = new ChromeDriver();
+        //System.setProperty("")
+        Thread.sleep(15000);
+        cd.quit();
+
+
+    }
+}
