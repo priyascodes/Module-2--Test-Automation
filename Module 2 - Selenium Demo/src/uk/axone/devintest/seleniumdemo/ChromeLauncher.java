8798a6bc3d48bf84b2df2de200d8731a13e20a94
Priya <priya3kr@gmail.com>
2020-10-03 17:01:38 +0100

    TestAutomation Project - Commiting Module 2 - Selenium ChromeLauncher Class 3

--- Module 2 - Selenium Demo/src/uk/axone/devintest/seleniumdemo/ChromeLauncher.java
+++ Module 2 - Selenium Demo/src/uk/axone/devintest/seleniumdemo/ChromeLauncher.java
@@ -8,7 +8,6 @@ public class ChromeLauncher {
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
 
         ChromeDriver cd = new ChromeDriver();
-        //System.setProperty("")
         Thread.sleep(15000);
         cd.quit();
 
