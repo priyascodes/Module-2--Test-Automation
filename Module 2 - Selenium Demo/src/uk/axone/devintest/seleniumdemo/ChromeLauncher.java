89c80ab14a093d9e7fd428d31be107f034782e1f
Priya <priya3kr@gmail.com>
2020-10-08 14:58:03 +0100

    ChromeLauncher, ChromeLauncher Test and Test.html

--- Module 2 - Selenium Demo/src/uk/axone/devintest/seleniumdemo/ChromeLauncher.java
+++ Module 2 - Selenium Demo/src/uk/axone/devintest/seleniumdemo/ChromeLauncher.java
@@ -1,13 +1,16 @@
 package uk.axone.devintest.seleniumdemo;
 
+import com.google.common.annotations.VisibleForTesting;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ChromeLauncher {
-
-    public static void main(String[] args) throws InterruptedException {
+    @Test
+    public void chromeLauncher() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
 
         ChromeDriver cd = new ChromeDriver();
+        cd.get("https://www.google.co.uk/");
         Thread.sleep(15000);
         cd.quit();
 
---
+++ Module 2 - Selenium Demo/src/uk/axone/devintest/seleniumdemo/ChromeLauncherTest.java
@@ -0,0 +1,13 @@
+package uk.axone.devintest.seleniumdemo;
+
+import org.junit.jupiter.api.Test;
+
+public class ChromeLauncherTest {
+
+    @Test
+            public void method() throws InterruptedException {
+        ChromeLauncher chrome = new ChromeLauncher();
+        chrome.chromeLauncher();
+
+    }
+}
